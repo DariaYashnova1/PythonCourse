409f624e4b69a4f257f4314c577196e4f674cbb7
DariaYashnova1 <128304896+DariaYashnova1@users.noreply.github.com>
2023-10-16 00:03:41 +0300

    Update task3.py

--- lab1/task3.py
+++ lab1/task3.py
@@ -1,7 +1,8 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
 len_ = len(list_players)
-team1 = list_players[:(len_)//2]
-team2 = list_players[(len_)//2:]
+mid = len_ // 2
+team1 = list_players[:mid]
+team2 = list_players[mid:]
 # TODO Разделите участников на две команды
 print(team1)
 print(team2)

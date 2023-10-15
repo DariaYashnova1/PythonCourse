a8258e455f9a52f13d9090e9d343cf7e9a1a3293
DariaYashnova1 <128304896+DariaYashnova1@users.noreply.github.com>
2023-10-16 00:02:50 +0300

    Update task1.py

--- lab1/task1.py
+++ lab1/task1.py
@@ -1,7 +1,7 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-
-position_ = numbers.index(None)
-summ_ = sum(numbers[0: position_])+sum(numbers[position_+1: len(numbers)+1])
-lenn_ = len(numbers)
-numbers[position_] = summ_/lenn_
+position = numbers.index(None)
+summ = sum(numbers[: position]) + sum(numbers[position + 1:])
+lenn = len(numbers)
+numbers[position] = summ / lenn
 print("Измененный список:", numbers)
+

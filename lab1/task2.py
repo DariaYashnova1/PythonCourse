0d3bf602d83a671477c709d5993f60e4b5bd3a34
DariaYashnova1 <128304896+DariaYashnova1@users.noreply.github.com>
2023-10-16 00:03:16 +0300

    Update task2.py

--- lab1/task2.py
+++ lab1/task2.py
@@ -1,9 +1,9 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
 
-volume = 1.44 * 1048576
+volume = 1.44 * 1024 * 1024
 pages = 100
 strs = 50
 sym = 25
 one_sym = 4
-res = volume//(pages*strs*sym*one_sym)
+res = volume // (pages * strs * sym * one_sym)
 print("Количество книг, помещающихся на дискету:", int(res))

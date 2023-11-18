45503db92462fa12f10feed8ad6545633522ccc7
DariaYashnova1 <128304896+DariaYashnova1@users.noreply.github.com>
2023-11-18 20:21:15 +0300

    Update task1.py

--- lab4/task1.py
+++ lab4/task1.py
@@ -1,18 +1,12 @@
-# TODO решите задачу
 import json
 
 IN_FILE = "input.json"
-
 
 def task(file_path) -> float:
     with open(file_path, 'r') as file:
         data = json.load(file)
 
-    product_sum = 0
-    for dictionary in data:
-        score = dictionary.get("score", 0)
-        weight = dictionary.get("weight", 0)
-        product_sum += score * weight
+    product_sum = sum(dictionary.get("score", 0) * dictionary.get("weight", 0) for dictionary in data)
 
     return round(product_sum, 3)
 
